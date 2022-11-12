736e8dc8b671d1972d6ac3e92571dcce0cc3d12e
ajin212 <antondoroshko95@gmail.com>
2022-11-12 17:51:07 +0300

    first
    first

---
+++ project1/3.py
@@ -0,0 +1,10 @@
+x = float(input())
+
+if x <= 60:
+    print("1")
+elif 64 >= x > 60:
+    print("2")
+elif 69 >= x > 64:
+    print("3")
+elif x > 69:
+    print("4")
---
+++ project1/4.py
@@ -0,0 +1,16 @@
+d = int(input())-1
+dname = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
+if d==0:
+    print (dname[0])
+elif d==1:
+    print (dname[1])
+elif d==2:
+    print (dname[2])
+elif d==3:
+    print (dname[3])
+elif d==4:
+    print (dname[4])
+elif d==5:
+    print (dname[5])
+elif d==6:
+    print (dname[6])
